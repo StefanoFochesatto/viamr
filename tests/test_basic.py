c903caf1ca49c6f3d5d7adb429d772aaa29589cb
Ed Bueler <elbueler@alaska.edu>
2026-01-09 12:17:40 -0800

    more structure to tests/; no content change

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,6 @@
 import numpy as np
-import pytest
 from firedrake import *
 from viamr import VIAMR
-import subprocess
-import os
-import pathlib
 
 
 def _get_netgen_mesh(TriHeight=0.4, width=2):
@@ -74,181 +70,6 @@ def test_unionmarks():
     discmark = Function(DG0).interpolate(_get_ball_obstacle(x, y) > 0.0)
     mark = amr.unionmarks(rightmark, discmark)
     assert abs(assemble(mark * dx) - 9.92) < 1.0e-10  # union of marked area
-
-
-def test_refine_udo():
-    mesh = _get_netgen_mesh(TriHeight=1.2)
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 19
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi, 0.0))
-    unorm0 = norm(u)
-    mark = amr.udomark(u, psi)
-    assert amr.countmark(mark) == 24
-    rmesh = mesh.refine_marked_elements(mark)  # netgen's refine method
-    rCG1, _ = amr.spaces(rmesh)
-    assert rCG1.dim() == 61
-    rV = FunctionSpace(rmesh, "CG", 1)
-    ru = Function(rV).interpolate(u)  # cross-mesh interpolation
-    assert abs(norm(ru) - unorm0) < 1.0e-10  # ... should be conservative
-
-
-def test_refine_vcd():
-    mesh = _get_netgen_mesh(TriHeight=1.2)
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 19
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi, 0.0))
-    unorm0 = norm(u)
-    mark = amr.vcdmark(u, psi)
-    rmesh = mesh.refine_marked_elements(mark)  # netgen's refine method
-    rCG1, _ = amr.spaces(rmesh)
-    assert rCG1.dim() == 49
-    rV = FunctionSpace(rmesh, "CG", 1)
-    ru = Function(rV).interpolate(u)  # cross-mesh interpolation
-    assert abs(norm(ru) - unorm0) < 1.0e-10  # ... should be conservative
-
-
-def test_adapt_avm():
-    mesh = RectangleMesh(8, 8, 2.0, 2.0, originX=-2.0, originY=-2.0)
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 81
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi, 0.0))
-    amr.setmetricparameters(target_complexity=100, h_min=1.0e-4, h_max=1.0)
-    rmesh = amr.adaptaveragedmetric(mesh, u, psi)
-    rCG1, _ = amr.spaces(rmesh)
-    assert rCG1.dim() == 152
-
-
-def test_adapt_avm_separated():
-    mesh = RectangleMesh(7, 7, 2.0, 2.0, originX=-2.0, originY=-2.0)
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 64
-    psi = Function(CG1).interpolate(Constant(0.0))
-    (x, y) = SpatialCoordinate(mesh)
-    r = sqrt(x**2 + y**2)
-    u_ufl = conditional(r < 1, 1.0 + cos(pi * r), 0.0)
-    uh = Function(CG1).interpolate(u_ufl)
-    amr.setmetricparameters(target_complexity=100, h_min=1.0e-4, h_max=1.0)
-    # only isotropic free-boundary metric
-    fbmesh = amr.adaptaveragedmetric(mesh, uh, psi, gamma=1.0)
-    fbCG1, _ = amr.spaces(fbmesh)
-    assert fbCG1.dim() == 129
-    # only hessian metric
-    hmesh = amr.adaptaveragedmetric(mesh, uh, psi, gamma=0.0)
-    hCG1, _ = amr.spaces(hmesh)
-    assert hCG1.dim() == 150
-
-
-def test_adapt_avm_intersect():
-    mesh = RectangleMesh(8, 8, 2.0, 2.0, originX=-2.0, originY=-2.0)
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 81
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi, 0.0))
-    amr.setmetricparameters(target_complexity=100, h_min=1.0e-4, h_max=1.0)
-    rmesh = amr.adaptaveragedmetric(mesh, u, psi, intersect=True)
-    rCG1, _ = amr.spaces(rmesh)
-    assert rCG1.dim() == 189
-
-
-def test_petsc4py_refine_vcd():
-    mesh = _get_netgen_mesh(TriHeight=1.2)
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 19
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi, 0.0))
-    unorm0 = norm(u)
-    mark = amr.vcdmark(u, psi)
-    rmesh = amr.refinemarkedelements(mesh, mark)
-    rCG1, _ = amr.spaces(rmesh)
-    assert rCG1.dim() == 49
-    rV = FunctionSpace(rmesh, "CG", 1)
-    ru = Function(rV).interpolate(u)  # cross-mesh interpolation
-    assert abs(norm(ru) - unorm0) < 1.0e-10  # ... should be conservative
-
-
-def test_refine_vcd_petsc4py_firedrake():
-    mesh = RectangleMesh(
-        5, 5, 2.0, 2.0, originX=-2.0, originY=-2.0
-    )  # Firedrake utility mesh, not netgen
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 36
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi, 0.0))
-    unorm0 = norm(u)
-    mark = amr.vcdmark(u, psi)
-    rmesh = amr.refinemarkedelements(mesh, mark)
-    rCG1, _ = amr.spaces(rmesh)
-    # check that direct solver gets same result
-    markDS = amr.vcdmark(u, psi, directsolver=True)
-    rmeshDS = amr.refinemarkedelements(mesh, markDS)
-    rCG1DS, _ = amr.spaces(rmeshDS)
-    assert rCG1DS.dim() == rCG1.dim() == 73
-    assert errornorm(mark, markDS) < 1.0e-14
-    # check conservative cross-mesh interpolation
-    rV = FunctionSpace(rmesh, "CG", 1)
-    ru = Function(rV).interpolate(u)
-    assert abs(norm(ru) - unorm0) < 1.0e-10
-
-
-def test_gradrecinactivemark():
-    mesh = RectangleMesh(6, 6, 2.0, 2.0, originX=-2.0, originY=-2.0, diagonal="crossed")
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 85
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi + 1.0, psi))
-    imark, _, _ = amr.gradrecinactivemark(u, psi, theta=0.5)
-    rmesh = amr.refinemarkedelements(mesh, imark)
-    rCG1, _ = amr.spaces(rmesh)
-    assert rCG1.dim() == 165
-
-
-def test_brinactivemark():
-    mesh = RectangleMesh(8, 8, 2.0, 2.0, originX=-2.0, originY=-2.0)
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 81
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi + 1.0, psi))
-    residual = -div(grad(u))  # largest near circle psi==0
-    (imark, _, _) = amr.brinactivemark(u, psi, residual, theta=0.8)
-    rmesh = amr.refinemarkedelements(mesh, imark)
-    # VTKFile(f"result_brinactivemark_refined.pvd").write(rmesh)
-    rCG1, _ = amr.spaces(rmesh)
-    assert rCG1.dim() == 147
-
-
-def test_brinactivemark_total():
-    mesh = RectangleMesh(8, 8, 2.0, 2.0, originX=-2.0, originY=-2.0)
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 81
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi + 1.0, psi))
-    residual = -div(grad(u))  # largest near circle psi==0
-    (imark, _, _) = amr.brinactivemark(u, psi, residual, theta=0.8, method="total")
-    rmesh = amr.refinemarkedelements(mesh, imark)
-    rCG1, _ = amr.spaces(rmesh)
-    assert rCG1.dim() == 154
 
 
 def test_overlapping_jaccard():
@@ -329,18 +150,9 @@ if __name__ == "__main__":
     test_spaces_sizes()
     test_mark_none()
     test_unionmarks()
-    test_refine_udo()
-    test_refine_vcd()
-    test_adapt_avm()
-    test_adapt_avm_separated()
-    test_adapt_avm_intersect()
-    test_brinactivemark()
-    test_brinactivemark_total()
     test_overlapping_jaccard()
     test_nonoverlapping_jaccard()
     test_symmetry_jaccard()
     test_jaccard_submesh_uniform()
     test_third_jaccard_ufl()
     test_overlapping_and_nonoverlapping_hausdorff()
-    test_petsc4py_refine_vcd()
-    test_refine_vcd_petsc4py_firedrake()
