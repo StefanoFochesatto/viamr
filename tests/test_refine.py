2d39fc4bd5cffca5261f421c7a11aca1e9d01879
Ed Bueler <elbueler@alaska.edu>
2026-01-09 20:43:42 -0800

    better names for, and organization of, refine/adapt tests

--- tests/test_refine.py
+++ tests/test_refine.py
@@ -41,6 +41,95 @@ def test_refine_vcd():
     assert abs(norm(ru) - unorm0) < 1.0e-10  # ... should be conservative
 
 
+def test_refine_vcd_petscsbr():
+    mesh = _get_netgen_mesh(TriHeight=1.2)
+    amr = VIAMR(debug=True)
+    CG1, _ = amr.spaces(mesh)
+    assert CG1.dim() == 19
+    (x, y) = SpatialCoordinate(mesh)
+    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
+    u = Function(CG1).interpolate(conditional(psi > 0.0, psi, 0.0))
+    unorm0 = norm(u)
+    mark = amr.vcdmark(u, psi)
+    rmesh = amr.refinemarkedelements(mesh, mark)  # PETSc's skeleton-based refine method
+    rCG1, _ = amr.spaces(rmesh)
+    assert rCG1.dim() == 49
+    rV = FunctionSpace(rmesh, "CG", 1)
+    ru = Function(rV).interpolate(u)  # cross-mesh interpolation
+    assert abs(norm(ru) - unorm0) < 1.0e-10  # ... should be conservative
+
+
+def test_refine_vcd_firedrake_petscsbr():
+    mesh = RectangleMesh(
+        5, 5, 2.0, 2.0, originX=-2.0, originY=-2.0
+    )  # Firedrake utility mesh, not netgen
+    amr = VIAMR(debug=True)
+    CG1, _ = amr.spaces(mesh)
+    assert CG1.dim() == 36
+    (x, y) = SpatialCoordinate(mesh)
+    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
+    u = Function(CG1).interpolate(conditional(psi > 0.0, psi, 0.0))
+    unorm0 = norm(u)
+    mark = amr.vcdmark(u, psi)
+    rmesh = amr.refinemarkedelements(mesh, mark)  # PETSc's skeleton-based refine method
+    rCG1, _ = amr.spaces(rmesh)
+    # check that direct solver gets same result
+    markDS = amr.vcdmark(u, psi, directsolver=True)
+    rmeshDS = amr.refinemarkedelements(mesh, markDS)
+    rCG1DS, _ = amr.spaces(rmeshDS)
+    assert rCG1DS.dim() == rCG1.dim() == 73
+    assert errornorm(mark, markDS) < 1.0e-14
+    # check conservative cross-mesh interpolation
+    rV = FunctionSpace(rmesh, "CG", 1)
+    ru = Function(rV).interpolate(u)
+    assert abs(norm(ru) - unorm0) < 1.0e-10
+
+
+def test_refine_gr():
+    mesh = RectangleMesh(6, 6, 2.0, 2.0, originX=-2.0, originY=-2.0, diagonal="crossed")
+    amr = VIAMR(debug=True)
+    CG1, _ = amr.spaces(mesh)
+    assert CG1.dim() == 85
+    (x, y) = SpatialCoordinate(mesh)
+    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
+    u = Function(CG1).interpolate(conditional(psi > 0.0, psi + 1.0, psi))
+    imark, _, _ = amr.gradrecinactivemark(u, psi, theta=0.5)
+    rmesh = amr.refinemarkedelements(mesh, imark)
+    rCG1, _ = amr.spaces(rmesh)
+    assert rCG1.dim() == 165
+
+
+def test_refine_br():
+    mesh = RectangleMesh(8, 8, 2.0, 2.0, originX=-2.0, originY=-2.0)
+    amr = VIAMR(debug=True)
+    CG1, _ = amr.spaces(mesh)
+    assert CG1.dim() == 81
+    (x, y) = SpatialCoordinate(mesh)
+    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
+    u = Function(CG1).interpolate(conditional(psi > 0.0, psi + 1.0, psi))
+    residual = -div(grad(u))  # largest near circle psi==0
+    (imark, _, _) = amr.brinactivemark(u, psi, residual, theta=0.8)
+    rmesh = amr.refinemarkedelements(mesh, imark)
+    # VTKFile(f"result_brinactivemark_refined.pvd").write(rmesh)
+    rCG1, _ = amr.spaces(rmesh)
+    assert rCG1.dim() == 147
+
+
+def test_refine_br_total():
+    mesh = RectangleMesh(8, 8, 2.0, 2.0, originX=-2.0, originY=-2.0)
+    amr = VIAMR(debug=True)
+    CG1, _ = amr.spaces(mesh)
+    assert CG1.dim() == 81
+    (x, y) = SpatialCoordinate(mesh)
+    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
+    u = Function(CG1).interpolate(conditional(psi > 0.0, psi + 1.0, psi))
+    residual = -div(grad(u))  # largest near circle psi==0
+    (imark, _, _) = amr.brinactivemark(u, psi, residual, theta=0.8, method="total")
+    rmesh = amr.refinemarkedelements(mesh, imark)
+    rCG1, _ = amr.spaces(rmesh)
+    assert rCG1.dim() == 154
+
+
 def test_adapt_avm():
     mesh = RectangleMesh(8, 8, 2.0, 2.0, originX=-2.0, originY=-2.0)
     amr = VIAMR(debug=True)
@@ -90,103 +179,14 @@ def test_adapt_avm_intersect():
     assert rCG1.dim() == 189
 
 
-def test_petsc4py_refine_vcd():
-    mesh = _get_netgen_mesh(TriHeight=1.2)
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 19
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi, 0.0))
-    unorm0 = norm(u)
-    mark = amr.vcdmark(u, psi)
-    rmesh = amr.refinemarkedelements(mesh, mark)
-    rCG1, _ = amr.spaces(rmesh)
-    assert rCG1.dim() == 49
-    rV = FunctionSpace(rmesh, "CG", 1)
-    ru = Function(rV).interpolate(u)  # cross-mesh interpolation
-    assert abs(norm(ru) - unorm0) < 1.0e-10  # ... should be conservative
-
-
-def test_refine_vcd_petsc4py_firedrake():
-    mesh = RectangleMesh(
-        5, 5, 2.0, 2.0, originX=-2.0, originY=-2.0
-    )  # Firedrake utility mesh, not netgen
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 36
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi, 0.0))
-    unorm0 = norm(u)
-    mark = amr.vcdmark(u, psi)
-    rmesh = amr.refinemarkedelements(mesh, mark)
-    rCG1, _ = amr.spaces(rmesh)
-    # check that direct solver gets same result
-    markDS = amr.vcdmark(u, psi, directsolver=True)
-    rmeshDS = amr.refinemarkedelements(mesh, markDS)
-    rCG1DS, _ = amr.spaces(rmeshDS)
-    assert rCG1DS.dim() == rCG1.dim() == 73
-    assert errornorm(mark, markDS) < 1.0e-14
-    # check conservative cross-mesh interpolation
-    rV = FunctionSpace(rmesh, "CG", 1)
-    ru = Function(rV).interpolate(u)
-    assert abs(norm(ru) - unorm0) < 1.0e-10
-
-
-def test_gradrecinactivemark():
-    mesh = RectangleMesh(6, 6, 2.0, 2.0, originX=-2.0, originY=-2.0, diagonal="crossed")
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 85
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi + 1.0, psi))
-    imark, _, _ = amr.gradrecinactivemark(u, psi, theta=0.5)
-    rmesh = amr.refinemarkedelements(mesh, imark)
-    rCG1, _ = amr.spaces(rmesh)
-    assert rCG1.dim() == 165
-
-
-def test_brinactivemark():
-    mesh = RectangleMesh(8, 8, 2.0, 2.0, originX=-2.0, originY=-2.0)
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 81
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi + 1.0, psi))
-    residual = -div(grad(u))  # largest near circle psi==0
-    (imark, _, _) = amr.brinactivemark(u, psi, residual, theta=0.8)
-    rmesh = amr.refinemarkedelements(mesh, imark)
-    # VTKFile(f"result_brinactivemark_refined.pvd").write(rmesh)
-    rCG1, _ = amr.spaces(rmesh)
-    assert rCG1.dim() == 147
-
-
-def test_brinactivemark_total():
-    mesh = RectangleMesh(8, 8, 2.0, 2.0, originX=-2.0, originY=-2.0)
-    amr = VIAMR(debug=True)
-    CG1, _ = amr.spaces(mesh)
-    assert CG1.dim() == 81
-    (x, y) = SpatialCoordinate(mesh)
-    psi = Function(CG1).interpolate(_get_ball_obstacle(x, y))
-    u = Function(CG1).interpolate(conditional(psi > 0.0, psi + 1.0, psi))
-    residual = -div(grad(u))  # largest near circle psi==0
-    (imark, _, _) = amr.brinactivemark(u, psi, residual, theta=0.8, method="total")
-    rmesh = amr.refinemarkedelements(mesh, imark)
-    rCG1, _ = amr.spaces(rmesh)
-    assert rCG1.dim() == 154
-
-
 if __name__ == "__main__":
     test_refine_udo()
     test_refine_vcd()
+    test_refine_vcd_petscsbr()
+    test_refine_vcd_firedrake_petscsbr()
+    test_refine_gr()
+    test_refine_br()
+    test_refine_br_total()
     test_adapt_avm()
     test_adapt_avm_separated()
     test_adapt_avm_intersect()
-    test_petsc4py_refine_vcd()
-    test_refine_vcd_petsc4py_firedrake()
-    test_gradrecinactivemark()
-    test_brinactivemark()
-    test_brinactivemark_total()
