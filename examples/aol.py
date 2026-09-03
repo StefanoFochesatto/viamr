cc8f5fdd17fdc30c9299c06e88569df3a63f728c
Ed Bueler <elbueler@alaska.edu>
2026-09-03 14:36:23 -0400

    change the UDO level to n=0 in the this demo example

--- examples/aol.py
+++ examples/aol.py
@@ -19,7 +19,7 @@ INF = Function(V).interpolate(Constant(PETSc.INFINITY))
 solver.solve(bounds=(psih, INF))
 
 amr = VIAMR()
-mark = amr.udomark(uh, psih, n=1)
+mark = amr.udomark(uh, psih, n=0)
 VTKFile("mesh.pvd").write(uh, mark)
 
 refinedmesh = amr.refinesbr2D(mesh, mark)
